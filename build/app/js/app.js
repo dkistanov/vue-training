ef6af79b7ca59ab13ee73f42b3bbb2e271284de6
Kistanov <rizzydizzy@mail.ru>
2022-02-03 23:27:12 +0300

    remove

--- build/app/js/app.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function () {
-    svg4everybody({});
-});
-// Библиотека wow.js для анимации
-
-(function () {
-	new WOW().init();
-})();
--- gulp/paths/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = [
-  './dev/static/js/app.js',
-  './dev/static/js/libs/svg4everybody.js',
-  './dev/static/js/libs/wowjs.js',
-];
